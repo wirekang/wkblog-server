99678aff7f1f52404f3d773fada5c8d07d071751
wirekang <wirekang@gmail.com>
2020-12-24 10:57:31 +0900

    getHash, getSHash 추가
    getHash는 단순한 해시값을 구할 때 쓰여서 sha1을 사용함.
    getSHash는 민감한 정보의 해시값을 구할 때 쓰여서 sha256에 추가 key를 넣어서 사용함.

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,17 @@
+import crypto from 'crypto';
+import Ignore from 'Ignore';
+
 export default {
 
   makeLog(head:string): (arg0: string)=>void {
     return (str:string) => (console.log(`[${head}] ${str}`));
+  },
+
+  getHash(str:string): string {
+    return crypto.createHash('sha1').update(str).digest('hex');
+  },
+
+  getSHash(str:string): string {
+    return crypto.createHmac('sha256', Ignore.secret).update(str).digest('hex');
   },
 };
