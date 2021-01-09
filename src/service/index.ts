840460412271d6b70e960370be804117b45639f9
wirekang <wirekang@gmail.com>
2021-01-09 23:22:49 +0900

    import 최적화

--- src/service/index.ts
+++ src/service/index.ts
@@ -8,32 +8,46 @@ enum Permission{
 }
 
 @injectable()
-export default class MyService implements Service {
-  @inject(TYPES.Auth) private auth!: Auth;
+export default class MyService implements I.Service {
+  @inject(TYPES.Auth) private auth!: I.Auth;
 
-  @inject(TYPES.Dao) private dao!: Dao;
+  @inject(TYPES.Dao) private dao!: I.Dao;
 
-  private permMap!: Map<ActionType, Permission>;
+  private permMap!: Map<I.ActionType, Permission>;
 
   constructor() {
     this.permMap = new Map();
-    this.permMap.set(ActionType.CreatePost, Permission.Admin);
-    this.permMap.set(ActionType.UpdatePost, Permission.Admin);
-    this.permMap.set(ActionType.DeletePost, Permission.Admin);
-    this.permMap.set(ActionType.PublishPost, Permission.Admin);
-    this.permMap.set(ActionType.Logout, Permission.Admin);
+    this.permMap.set(I.ActionType.CreatePost, Permission.Admin);
+    this.permMap.set(I.ActionType.UpdatePost, Permission.Admin);
+    this.permMap.set(I.ActionType.DeletePost, Permission.Admin);
+    this.permMap.set(I.ActionType.PublishPost, Permission.Admin);
+    this.permMap.set(I.ActionType.Logout, Permission.Admin);
   }
 
-  private getPermission(type: ActionType): Permission {
+  private getPermission(type: I.ActionType): Permission {
     return this.permMap.get(type) || Permission.Anyone;
   }
 
-  do<A extends Action<ActionType, unknown, unknown>>(
+  async do<A extends I.Action<I.ActionType, unknown, unknown>>(
     type: A['type'], input: A['input'], hash: string,
   ): Promise<A['output']> {
     if (this.getPermission(type) === Permission.Admin) {
       this.auth.validate(hash);
     }
-    return this.dao.do(type, input, this.auth.isLogin(hash));
+    switch (type) {
+      case I.ActionType.Login: {
+        const loginInput = input as I.LoginInput;
+        const hash = this.auth.login(loginInput.id, loginInput.pw);
+        const loginOutput:I.LoginOutput = { hash };
+        return loginOutput;
+      }
+      case I.ActionType.Logout: {
+        this.auth.logout(hash);
+        return null;
+      }
+      default: {
+        return this.dao.do(type, input, this.auth.isLogin(hash));
+      }
+    }
   }
 }
