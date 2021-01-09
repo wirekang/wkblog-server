20285d6953421268cab043c50244a08cbbb8c7c8
wirekang <wirekang@gmail.com>
2021-01-09 20:44:18 +0900

    Auth 테스트

--- src/test/auth.test.ts
+++ src/test/auth.test.ts
@@ -1,15 +1,26 @@
 import 'reflect-metadata';
 import MyAuth from 'auth';
 import Config from 'Config';
+import { Auth } from 'interfaces';
 
 describe('Auth', () => {
-  Config.parse('.test-config');
-  const auth = new MyAuth();
-  it('login, validate expire', async () => {
-    const hash = auth.login('idid', 'pwpw');
+  Config.parse('.config.test.json');
+  const auth:Auth = new MyAuth();
+  let hash = '';
+  it('해시', () => {
+    console.log((auth as MyAuth).makeHash('id', 'pw'));
+  });
+  it('로그인', () => {
+    hash = auth.login('id', 'pw');
     expect(auth.isLogin(hash)).toBeTruthy();
     auth.validate(hash);
-    await new Promise<void>((r) => setTimeout(() => r(), 4901));
+  });
+  it('잘못된 인증', () => {
+    expect(auth.isLogin('w2ewwerasdfasdvw2ewfsscxv')).toBeFalsy();
+    expect(auth.validate.bind(null, 'asdfqwerksdfj')).toThrowError();
+  });
+  it('수명 확인', async () => {
+    await new Promise<void>((r) => setTimeout(() => r(), 1000));
     expect(auth.validate.bind(null, hash)).toThrowError();
   });
 });
