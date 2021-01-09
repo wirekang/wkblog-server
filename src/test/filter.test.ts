13283ec45b19616ba7e3d0c166d07f0c08d81bc2
wirekang <wirekang@gmail.com>
2021-01-09 23:06:27 +0900

    Filter 테스트

--- src/test/filter.test.ts
+++ src/test/filter.test.ts
@@ -1,10 +1,16 @@
+import 'reflect-metadata';
 import Filter from 'filter';
 
 describe('Filter', () => {
   const filter = new Filter();
-  it('escapeHTML', () => {
-    const input = '<h1>Hello</h1>\n<p>let str = "I am wirekang"';
-    expect(filter.escapeHTML(input)).toBe('&lt;h1&gt;Hello&lt;/h1&gt;\n'
+  it('html', () => {
+    const input = '<h1>Hello</h1>\n<p>let str = "I am "';
+    expect(filter.html(input)).toBe('&lt;h1&gt;Hello&lt;/h1&gt;\n'
     + '&lt;p&gt;let str = &quot;I am &quot;');
+  });
+
+  it('commentName', () => {
+    const input = 'zwirekangz';
+    expect(filter.commentName(input)).toBe('zz');
   });
 });
