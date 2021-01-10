f4edeae05df386ca13527e7ad7bb1e9a739a970c
wirekang <wirekang@gmail.com>
2021-01-10 20:29:01 +0900

    serverPort 추가

--- src/Config.ts
+++ src/Config.ts
@@ -18,6 +18,8 @@ class Config {
 
   maxAge!: number;
 
+  serverPort!: number;
+
   parse(configFilePath: string) {
     const cfg = fs.readFileSync(
       path.resolve(process.cwd(), configFilePath),
@@ -25,7 +27,7 @@ class Config {
     Object.assign(this, JSON.parse(cfg.toString()));
     console.log('Config loaded');
     if (!(this.host && this.port && this.username && this.password
-      && this.database && this.key && this.hash)) {
+      && this.database && this.key && this.hash && this.serverPort)) {
       throw new Error(`Config: ${this}`);
     }
   }
