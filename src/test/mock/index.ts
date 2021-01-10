99984b59ce845d64e651f696f63e67d8bb2282aa
wirekang <wirekang@gmail.com>
2021-01-10 10:07:33 +0900

    LimiterMock default export

--- src/test/mock/index.ts
+++ src/test/mock/index.ts
@@ -2,3 +2,4 @@ export { default as AuthMock } from 'test/mock/auth';
 export { default as DaoMock } from 'test/mock/dao';
 export { default as FilterMock } from 'test/mock/filter';
 export { default as ServiceMock } from 'test/mock/service';
+export { default as LimiterMock } from 'test/mock/limiter';
