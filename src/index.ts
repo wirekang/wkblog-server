ccfa8ba0fb26ebd46d9fff2f0f3556bfe166bedf
wirekang <wirekang@gmail.com>
2021-01-16 08:48:24 +0900

    import * as I from 'interfaces'

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
 import 'reflect-metadata';
 import Option from 'Option';
-import {
-  Auth,
-  Converter, Dao, Filter, Limiter, Server, Service,
-} from 'interfaces';
+import * as I from 'interfaces';
 import container from 'inversify.config';
 import TYPES from 'Types';
 
 (async () => {
-  const dao = container.get<Dao>(TYPES.Dao);
-  const server = container.get<Server>(TYPES.Server);
-  const limiter = container.get<Limiter>(TYPES.Limiter);
-  const converter = container.get<Converter>(TYPES.Converter);
-  const auth = container.get<Auth>(TYPES.Auth);
-  const service = container.get<Service>(TYPES.Service);
-  const filter = container.get<Filter>(TYPES.Filter);
+  const dao = container.get<I.Dao>(TYPES.Dao);
+  const server = container.get<I.Server>(TYPES.Server);
+  const limiter = container.get<I.Limiter>(TYPES.Limiter);
+  const converter = container.get<I.Converter>(TYPES.Converter);
+  const auth = container.get<I.Auth>(TYPES.Auth);
+  const service = container.get<I.Service>(TYPES.Service);
+  const filter = container.get<I.Filter>(TYPES.Filter);
   const configPath = process.argv[2] || '.config.json';
 
   auth.init(Option.auth());
   limiter.init(Option.limiter());
   dao.init(Option.dao());
   server.init(Option.server());
+
   await dao.connect();
   await server.open();
 })();
