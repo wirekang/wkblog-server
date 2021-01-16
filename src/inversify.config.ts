d12b565f902295244c9455784ceb2a5b50d8a7ec
wirekang <wirekang@gmail.com>
2021-01-16 12:52:07 +0900

    index대신 inversify.config에서 init

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -8,6 +8,7 @@ import MyLimiter from 'limiter';
 import MyServer from 'server';
 import MyService from 'service';
 import TYPES from 'Types';
+import Option from 'Option';
 
 const container = new Container();
 container.bind<I.Service>(TYPES.Service).to(MyService).inSingletonScope();
@@ -17,4 +18,17 @@ container.bind<I.Auth>(TYPES.Auth).to(MyAuth).inSingletonScope();
 container.bind<I.Filter>(TYPES.Filter).to(MyFilter).inSingletonScope();
 container.bind<I.Limiter>(TYPES.Limiter).to(MyLimiter).inSingletonScope();
 container.bind<I.Converter>(TYPES.Converter).to(MyConverter).inSingletonScope();
+
+const auth = container.get<I.Auth>(TYPES.Auth);
+auth.init(Option.auth());
+
+const dao = container.get<I.Dao>(TYPES.Dao);
+dao.init(Option.dao());
+
+const server = container.get<I.Server>(TYPES.Server);
+server.init(Option.server());
+
+const limiter = container.get<I.Limiter>(TYPES.Limiter);
+limiter.init(Option.limiter());
+
 export default container;
