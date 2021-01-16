4982e32a34de4207b49a5b037c5e20c86c82c693
wirekang <wirekang@gmail.com>
2021-01-16 16:37:11 +0900

    임시 log 삭제

--- src/server/index.ts
+++ src/server/index.ts
@@ -68,9 +68,7 @@ export default class MyServer implements I.Server {
   private setRoutes():void {
     this.router.post('/api', async (ctx) => {
       try {
-        console.log(ctx.header);
         const body = ctx.request.body as Body;
-        console.log(body);
         const action = I.ActionType[body.action];
         if (action === undefined) {
           utils.log('ActionUndef', `body: ${JSON.stringify(body)}`);
